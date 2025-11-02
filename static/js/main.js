602fbc56f4fbace084381ebcd5ff3f5af1529c3e
amayum <a.khalid3@newcastle.ac.uk>
2025-11-02 03:11:17 +0000

    updated main.js

---
+++ static/js/main.js
@@ -0,0 +1,14 @@
+// get DOM elements
+const startBtn = document.getElementById("start-btn");
+const homeScreen = document.getElementById("home-screen");
+
+// add click listener to Start Quiz button
+startBtn.addEventListener("click", () => {
+  // play fade-out animation
+  homeScreen.classList.add("fade-out");
+
+  // after animation completes, redirect to quiz page
+  setTimeout(() => {
+    window.location.href = "quiz.html";
+  }, 800); 
+});
