859b164f9e3c443cddad8ba196b76dd34126b4cc
amayum <a.khalid3@newcastle.ac.uk>
2025-11-02 11:55:55 +0000

    fixed files

--- static/main.js
+++ static/main.js
@@ -3,14 +3,12 @@ const startBtn = document.getElementById("start-btn");
 const homeScreen = document.getElementById("home-screen");
 
 // add click listener to Start Quiz button
-startBtn.addEventListener("click", (e) => {
-  e.preventDefault(); // prevent default link behavior
-
+startBtn.addEventListener("click", () => {
   // play fade-out animation
   homeScreen.classList.add("fade-out");
 
   // after animation completes, redirect to quiz page
   setTimeout(() => {
-    window.location.href = "quiz.html";
+    window.location.href = "/quiz";
   }, 800); 
 });
